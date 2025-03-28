5d0d1161357bcbcd6333fe329e59d64b4655a69c
andres naranjo <santo2828@gmail.com>
2025-03-27 22:18:01 -0500

    implemntación QR por accion y el ID

--- src/app/feature/pages/programador/programador.component.ts
+++ src/app/feature/pages/programador/programador.component.ts
@@ -1,13 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { QRCodeComponent,  } from 'angularx-qrcode';
 
 @Component({
   selector: 'app-programador',
-  imports: [BsDatepickerModule, FormsModule],
+  imports: [CommonModule, QRCodeComponent, FormsModule],
   templateUrl: './programador.component.html',
   styleUrl: './programador.component.css'
 })
 export class ProgramadorComponent {
-  fechaSeleccionada: Date | null = null;
+
+  enviarQR() {//Todo: Implementar la logica para enviar el QR al backend
+  
+    throw new Error('Method not implemented.');
+  }
+
+  descargarQR() {
+    
+  }
+
+  imprimirQR() {
+  throw new Error('Method not implemented.');
+  }
+
+  accion: 'activar' | 'finalizar' = 'activar';
+  idDesafio: string = '';
+  codigoQR: string = '';
+
+  generarQR() {
+    if (this.idDesafio.trim()) {
+      // generar el codigo QR en formato JSON y asignarlo a la variable codigoQR
+      this.codigoQR = JSON.stringify({
+        accion: this.accion,
+        idDesafio: this.idDesafio.trim()
+      });
+    } else {
+      alert('Por favor escribe un ID de desafío.');
+    }
+  }
+
 }
