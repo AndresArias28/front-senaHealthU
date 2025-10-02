b311fd196423fb762b2e6d5c67abca88260681a1
santiagomera06 <166671029+santiagomera06@users.noreply.github.com>
2025-10-02 14:29:25 -0500

    mapbox

--- src/app/feature/pages/contact/contact.component.ts
+++ src/app/feature/pages/contact/contact.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
+import mapboxgl from 'mapbox-gl';
+import { environment } from '../../../../environments/environmets.mapbox';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit, AfterViewInit, OnDestroy {
+
+  @ViewChild('map') mapContainer!: ElementRef;
+  map!: mapboxgl.Map;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    (mapboxgl as any).accessToken = environment.mapbox.accessToken;
+  }
+
+  ngAfterViewInit(): void {
+    this.map = new mapboxgl.Map({
+      container: this.mapContainer.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [-76.56177339999999, 2.4832482],
+      zoom: 15
+    });
+
+    this.map.addControl(new mapboxgl.NavigationControl());
+
+    new mapboxgl.Marker()
+      .setLngLat([-76.56177339999999, 2.4832482])
+      .addTo(this.map);
+  }
+
+  ngOnDestroy(): void {
+    this.map.remove();
+  }
 
 }
